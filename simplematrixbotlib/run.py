6c22ef44210353a75757528eb366a300db6098e1
Isaac Beverly <imbev@protonmail.com>
2024-05-03 22:45:12 +0000

    Fix default arg for run

--- simplematrixbotlib/run.py
+++ simplematrixbotlib/run.py
@@ -9,7 +9,7 @@ from .deps import Deps
 from .handler import Handler
 
 logger = logging.getLogger(__name__)
-
+"""@private"""
 
 async def run_coroutine(creds: Creds, handlers: Iterable[Handler], deps: Optional[Deps] = None):
     client = await creds.get_valid_client()
@@ -23,5 +23,5 @@ async def run_coroutine(creds: Creds, handlers: Iterable[Handler], deps: Optiona
         await client.close()
 
 
-def run(creds: Creds, handlers: Iterable[Handler], deps: Deps):
+def run(creds: Creds, handlers: Iterable[Handler], deps: Optional[Deps] = None):
     get_event_loop().run_until_complete(run_coroutine(creds, handlers, deps))
