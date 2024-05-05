7279060dd9c641cc92b857731105581b4692c5e9
Dreamsorcerer <dreamsorcerer@noreply.codeberg.org>
2024-05-05 22:20:03 +0000

    Type listener.py

--- simplematrixbotlib/listener.py
+++ simplematrixbotlib/listener.py
@@ -1,14 +1,19 @@
-from nio import RoomMessageText, ReactionEvent
+from typing import TYPE_CHECKING, Callable
+
+from nio import Event, MatrixRoom, RoomMessage, RoomMessageText, ReactionEvent
+
+if TYPE_CHECKING:
+    from simplematrixbotlib.bot import Bot
 
 
 class Listener:
 
-    def __init__(self, bot):
+    def __init__(self, bot: "Bot"):
         self._bot = bot
         self._registry = []
         self._startup_registry = []
 
-    def on_custom_event(self, event):
+    def on_custom_event(self, event: Event) -> Callable[[Callable[..., None]], None]:
 
         def wrapper(func):
             if [func, event] in self._registry:
@@ -18,20 +23,20 @@ class Listener:
 
         return wrapper
 
-    def on_message_event(self, func):
+    def on_message_event(self, func: Callable[[MatrixRoom, RoomMessageText], None]) -> None:
         if [func, RoomMessageText] in self._registry:
             func()
         else:
             self._registry.append([func, RoomMessageText])
 
-    def on_reaction_event(self, func):
+    def on_reaction_event(self, func: Callable[[MatrixRoom, ReactionEvent, str], None]) -> None:
 
         async def wrapper(room, event):
             await func(room, event, event.key)
 
         self._registry.append([wrapper, ReactionEvent])
 
-    def on_startup(self, func):
+    def on_startup(self, func: Callable[[str], None]) -> None:
         if func in self._startup_registry:
             func()
         else:
