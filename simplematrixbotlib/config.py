3345de1af9a2bc3774e3c8bbbced4985aff7a9db
Isaac Beverly <imbev@protonmail.com>
2024-07-03 18:59:32 -0500

    Add docstrings for Config.load_toml, Config.load_json

--- simplematrixbotlib/config.py
+++ simplematrixbotlib/config.py
@@ -8,8 +8,11 @@ from logging import getLogger
 
 
 logger = getLogger(__name__)
+"""@private"""
+
 
 class InvalidConfig(ValueError):
+    """@private"""
     pass
 
 
@@ -37,17 +40,43 @@ class Config:
 
     @staticmethod
     def load_toml(file_path: str) -> Config:
+        """
+        Load config values from specified toml file.
+        Example:
+        ```python
+        config = Config.load_toml("config.toml")
+        ```
+        ```toml
+        [tool.simplematrixbotlib]
+        join_room_on_invite_enabled = false
+        ```
+        """
         with open(file_path, "rb") as f:
             data = tomllib.load(f)
         return Config._from_dict(data)
 
     @staticmethod
     def load_json(file_path: str) -> Config:
+        """
+        Load config values from specified json file.
+        Example:
+        ```python
+        config = Config.load_json("config.json")
+        ```
+        ```json
+        {
+          "tool.simplematrixbotlib": {
+            "join_room_on_invite_enabled": false
+          }
+        }
+        ```
+        """
         with open(file_path, "r") as f:
             data = json.load(f)
         return Config._from_dict(data)
 
     def log(self):
+        """@private"""
         for attr, default_value in Config().__dict__.items():
             value = self.__dict__.get(attr)
             if value != default_value:
