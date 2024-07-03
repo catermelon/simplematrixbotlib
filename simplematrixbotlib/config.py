0f04ff6403fdc1e0490647f8871f3e31b93b1f11
Isaac Beverly <imbev@protonmail.com>
2024-07-03 15:50:57 -0500

    Add Config load_json

--- simplematrixbotlib/config.py
+++ simplematrixbotlib/config.py
@@ -1,12 +1,16 @@
 from __future__ import annotations
 
 import logging
+import json
 import tomllib
 from dataclasses import dataclass
 from logging import getLogger
 
 
 logger = getLogger(__name__)
+
+class InvalidConfig(ValueError):
+    pass
 
 
 @dataclass
@@ -15,10 +19,33 @@ class Config:
     join_room_on_invite_enabled: bool = True
 
     @staticmethod
+    def _from_dict(data: dict) -> Config:
+        values: dict = {}
+
+        if data.get("tool.simplematrixbotlib"):
+            values = data.get("tool.simplematrixbotlib") #type: ignore
+
+        elif data.get("tool"):
+            tool_values = data.get("tool", {})
+            if tool_values.get("simplematrixbotlib"):
+                values = tool_values.get("simplematrixbotlib")
+
+        else:
+            raise InvalidConfig("Config values must be within tool.simplematrixbotlib")
+
+        return Config(**values)
+
+    @staticmethod
     def load_toml(file_path: str) -> Config:
         with open(file_path, "rb") as f:
             data = tomllib.load(f)
-        return Config(**data.get("tool").get("simplematrixbotlib"))
+        return Config._from_dict(data)
+
+    @staticmethod
+    def load_json(file_path: str) -> Config:
+        with open(file_path, "r") as f:
+            data = json.load(f)
+        return Config._from_dict(data)
 
     def log(self):
         for attr, default_value in Config().__dict__.items():
