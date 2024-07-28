6fe0c9eb3f3b1253add9e0ba2463a1d5727149f2
charlhakeem <charlhakeem@noreply.codeberg.org>
2024-07-28 20:45:37 +0000

    Add markdown to v3

--- simplematrixbotlib/room.py
+++ simplematrixbotlib/room.py
@@ -1,5 +1,7 @@
 from typing import Optional
 
+import markdown
+
 from nio import MatrixRoom, AsyncClient
 
 
@@ -14,16 +16,25 @@ class Room:
         self.client = client
         self.nio_room = room
 
-
     def __repr__(self):
         return f"{self.room_id}: {self.name}"
 
+    async def send_text(self, message_body: str, format_as_markdown: bool = False, reply_to_event_id: Optional[str] = None, room_id: Optional[str] = None):
+        if not room_id:
+            room_id = self.room_id
 
-    async def send_text(self, message_body: str, reply_to_event_id: Optional[str] = None):
         content = {
             "msgtype": "m.text",
             "body": message_body
         }
+
+        if format_as_markdown:
+            content.update({
+                "format": "org.matrix.custom.html",
+                "formatted_body": markdown.markdown(message_body,
+                                                    extensions=["sane_lists", "fenced_code", "nl2br"])
+            })
+
         if reply_to_event_id:
             try:
                 content["m.relates_to"]
@@ -32,12 +43,12 @@ class Room:
             content["m.relates_to"]["m.in_reply_to"] = { # type: ignore
                 "event_id": reply_to_event_id
             }
+
         await self.client.room_send(
-            room_id=self.room_id,
+            room_id=room_id,
             message_type="m.room.message",
             content=content
         )
-
 
     async def join(self):
         await self.client.join(self.room_id)
