bc16b63587578b5e598b35582095a9c3aed65888
Isaac Beverly <imbev@protonmail.com>
2024-07-30 19:35:13 -0500

    Remove simplematrixbotlib_old from v3 branch

--- simplematrixbotlib_old/__init__.py
+++
@@ -1,8 +0,0 @@
-from simplematrixbotlib_old.api import Api
-from simplematrixbotlib_old.bot import Bot, run, stop
-from simplematrixbotlib_old.callbacks import Callbacks
-from simplematrixbotlib_old.config import Config
-from simplematrixbotlib_old.creds import Creds
-from simplematrixbotlib_old.listener import on_message, on_reaction, on_event, on_start, on_end
-from simplematrixbotlib_old.match import MessageMatch, match
-from simplematrixbotlib_old.parser_ import parse, ParseError  # type: ignore
