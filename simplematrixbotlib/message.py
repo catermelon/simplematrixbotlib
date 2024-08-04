e266530b84d036e06ddf7c49314620574c782f07
charlhakeem <charlhakeem@noreply.codeberg.org>
2024-08-04 20:31:16 +0000

    Add contains to message

--- simplematrixbotlib/message.py
+++ simplematrixbotlib/message.py
@@ -1,4 +1,5 @@
 from nio import RoomMessageText
+import re
 
 
 class Message:
@@ -13,3 +14,19 @@ class Message:
 
     def __repr__(self):
         return self.body
+
+    def get_body(self):
+        if self.formatted_body and "<mx-reply>" in self.formatted_body:
+            return re.sub(r'<mx-reply>.*?</mx-reply>', '', self.formatted_body)
+        return self.body
+
+    def contains(self, string, regex: bool = False, case_sensitive: bool = True):
+        body = self.get_body() if case_sensitive else self.get_body().lower()
+
+        if regex:
+            return bool(re.search(string, body, 0 if case_sensitive else re.IGNORECASE))
+
+        if not isinstance(string, str):
+            return any(body in (a if case_sensitive else a.lower()) for a in string)
+
+        return (string if case_sensitive else string.lower()) in body
